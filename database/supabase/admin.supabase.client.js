f9a4ffbe16f004492b416a75ce65304dc4857587
toykam <kabirtoyyib19@gmail.com>
2023-08-20 20:28:46 +0100

    added checks for order-on-notify

--- database/supabase/admin.supabase.client.js
+++ database/supabase/admin.supabase.client.js
@@ -1,2 +1,8 @@
-const { supabase } = require("@supabase/supabase-js")
+const { createClient } = require("@supabase/supabase-js")
 
+const supabaseAdminClient = createClient(
+    process.env['SUPABASE_URL'],
+    process.env['SUPABASE_SECRET_KEY']
+)
+
+module.exports = {supabaseAdminClient}
--- routes/notify-on-order.js
+++ routes/notify-on-order.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
+const { supabaseAdminClient } = require("../database/supabase/admin.supabase.client");
 
 const notifyOnOrderRouter = Router()
 
 
-notifyOnOrderRouter.get("/notify-on-order", (req, res) => {
-    res.send("<h1>I will notify you on order</h1>")
+notifyOnOrderRouter.get("/notify-on-order", async (req, res) => {
+    const users = await supabaseAdminClient.from("users").select("*");
+    res.send({"message": "Users ::: ", users})
 })
 
-notifyOnOrderRouter.post("/notify-on-order", (req, res) => {
+notifyOnOrderRouter.post("/notify-on-order", async (req, res) => {
     try {
         const {
             type, record,
@@ -16,11 +18,19 @@ notifyOnOrderRouter.post("/notify-on-order", (req, res) => {
             status, cook, waiter
         } = record
 
+        const cookDetail = await supabaseAdminClient.from("users").select("*").eq("user_id", cook);
+        const waiterDetail = await supabaseAdminClient.from("users").select("*").eq("user_id", waiter);
+
+        console.log(cookDetail, waiterDetail);
+
         if (type == "INSERT") {
 
         } else if (type == "UPDATE") {
             
         }
+        res.send({
+            "message": "Al done"
+        })
     } catch (error) {
         res.status(500).send("Error: ", error)
     }
