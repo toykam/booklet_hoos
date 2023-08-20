828b040b35f840bc4a0de0cd11ff426f6e744266
toykam <kabirtoyyib19@gmail.com>
2023-08-20 20:51:15 +0100

    service account.js hidden

--- fcm/fcm.js
+++ fcm/fcm.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin")
 
-const serviceAccount = require("./service-account.json");
+const serviceAccount = require("./../service-account.json");
 const fcmApp = admin.initializeApp({
     credential: admin.credential.cert(
         serviceAccount
